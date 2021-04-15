f1534d86e254e0c0befaa78a7a23bc17ee8ee9e8
Joe Kim <joekim6a@gmail.com>
2021-04-15 13:54:46 -0700

    solved problem 1

--- test/phase-1-spec.js
+++ test/phase-1-spec.js
@@ -1,46 +1,47 @@
 const chai = require("chai");
 const expect = chai.expect;
 const { isFive, isOdd, myRange, fizzBuzz} = require("../problems/phase-1");
-describe("isFive(num)", () => { 
+describe("isFive(num)", () => {
   it("should return true if the num is 5 otherwise false", () => {
     //Arrange
+    let num = 5;
     //Act
+    let result = isFive(num);
+    let expected = 5;
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(result).to.equal(expected)
 
     //Arrange
-    
     //Act
-    
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
 
   });
 });
 describe("isOdd(number)", () => {
   it("should return true if number is odd", () => {
     //Arrange
-   
     //Act
-
+    const test = isOdd(5);
+    const result = true;
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(test).to.eql(result);
+
 
   });
   it("should return false if the num is even", () => {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
   });
   it("should throw an error if num is not type of Number", () => {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
@@ -50,9 +51,9 @@ describe("myRange(min, max, step)", () => {
   context("if step is not provided", () => {
     it("should return the correct array with default value step=1", () => {
       //Arrange
-      
+
       //Act
-      
+
       // Assert
       expect.fail('Remove this expect.fail and replace it with your test');
 
@@ -61,9 +62,9 @@ describe("myRange(min, max, step)", () => {
   context("if step is provided", () => {
     it("should return the correct array", () => {
       //Arrange
-     
+
       //Act
-     
+
       // Assert
       expect.fail('Remove this expect.fail and replace it with your test');
 
@@ -71,9 +72,9 @@ describe("myRange(min, max, step)", () => {
   });
   it("should throw an error if num is not type of Number", () => {
     //Arrange
-   
+
     //Act
-   
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
@@ -82,27 +83,27 @@ describe("myRange(min, max, step)", () => {
 describe('fizzBuzz(max)', ()=> {
   it('should throw a RangeError if max < 0', ()=> {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
   })
   it('should throw a TypeError if max is not a number', ()=> {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
   })
   it('should return an array from 0 to max (not inclusive) of numbers that are divisible by either 3 or 5 but not both', ()=> {
     //Arrange
-    
+
     //Act
-    
+
     //Assert
     expect.fail('Remove this expect.fail and replace it with your test');
 
